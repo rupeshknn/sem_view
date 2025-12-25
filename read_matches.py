f5bb83df6f06d7c824d10689141730cf60de5492
rupeshknn <rupeshknn@gmail.com>
2025-12-25 17:34:32 -0600

    chore: remove temp files and add .gitignore

--- read_matches.py
+++
@@ -1,9 +0,0 @@
-def read_matches():
-    with open("matches.txt", "r") as f:
-        lines = f.readlines()
-        for i, line in enumerate(lines):
-            if i < 50:
-                print(line.strip())
-
-if __name__ == "__main__":
-    read_matches()
